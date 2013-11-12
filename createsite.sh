b1c022792b34cebe503b97e9c7628e0c5da2fb77
Randall Puterbaugh <rputerbaugh@gmail.com>
2013-11-12 13:23:09 -0800

    Commented out many lines for troubleshooting

--- createsite.sh
+++ createsite.sh
@@ -40,28 +40,28 @@ perl -pi -e "s~\$databases = array\(\);~\$databases = array ( \n  'default' => \
 perl -pi -e "s~# .base_url = 'http://www.example.com';~\\\$base_url = 'http://\\$sitename\.cascadiaweb.net';~g" settings.php
 #
 # Create .htaccess inside of sites/default/files
-cd $www/$sitedomain/sites/default/files
-echo -n "SetHandler Drupal_Security_Do_Not_Remove_See_SA_2006_006
-Options None
-Options +FollowSymLinks" > .htaccess
+#cd $www/$sitedomain/sites/default/files
+#echo -n "SetHandler Drupal_Security_Do_Not_Remove_See_SA_2006_006
+#Options None
+#Options +FollowSymLinks" > .htaccess
 #
 # Create the file with VirtualHost configuration in /etc/apache2/site-available/
-echo "<VirtualHost *:80>
-        DirectoryIndex index.php
-        DocumentRoot $www/$sitedomain
-        ServerAdmin maintenance@hackrobats.net
-        ServerAlias $sitename.cascadiaweb.net $sitename.hackrobats.net
-        ServerName $sitedomain
-</VirtualHost>" > /etc/apache2/sites-available/$sitedomain
+#echo "<VirtualHost *:80>
+#        DirectoryIndex index.php
+#        DocumentRoot $www/$sitedomain
+#        ServerAdmin maintenance@hackrobats.net
+#        ServerAlias $sitename.cascadiaweb.net $sitename.hackrobats.net
+#        ServerName $sitedomain
+#</VirtualHost>" > /etc/apache2/sites-available/$sitedomain
 #
 # Enable the site
-a2ensite $sitedomain
+#a2ensite $sitedomain
 #
 # Reload Apache2
-service apache2 reload
+#service apache2 reload
 #
 # Restart Apache2
-service apache2 restart
+#service apache2 restart
 #
 # Execute Drush Make
 cd /$www/$sitedomain
