fe6c46af0a216fd0cae0b100410e73d15fe39e58
Randall Puterbaugh <rputerbaugh@gmail.com>
2013-11-12 17:56:24 -0800

    Added Comments

--- createsite.sh
+++ createsite.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
-
+#
+# Prompt user to enter Domain Name
 read -p "Domain Name: " domain
-
+# Create variables from Domain Name
 www=/var/www/drupal7
 name=`echo $domain |rev |cut -c 5-|rev`
 tld=`echo $domain |cut -c 5-`
 machine=`echo $name |tr '-' '_'`
 dbpw=$(pwgen -n 16)
-echo "Site name: $name 
-Created at: $www/$domain 
-Machine name: $machine 
-Password: $dbpw"
-
+# Create database and user
 db="create database $machine;GRANT ALL PRIVILEGES ON $machine.* TO $machine@localhost IDENTIFIED BY '$dbpw';FLUSH PRIVILEGES;"
 mysql -udeploy -e "$db"
-
+# Create directories necessary for Drupal installation
 mkdir $www/$domain $www/$domain/sites $www/$domain/sites/default $www/$domain/sites/default/files
 chmod 775 $www/$domain/sites/default/files
 cd $www/$domain/sites/default/files
 echo -n "SetHandler Drupal_Security_Do_Not_Remove_See_SA_2006_006
 Options None
 Options +FollowSymLinks" > .htaccess
-
+# Copy settings.php from github.com/drupal/*
 cd $www/$domain/sites/default
 curl -o $www/$domain/sites/default/settings.php 'https://raw.github.com/drupal/drupal/7.x/sites/default/default.settings.php'
 chmod 777 $www/$domain/sites/default/settings.php 
-
+# Populate database information in settings.php
 perl -pi -e "s~\$databases = array\(\);~\$databases = array ( \n  'default' => \n  array ( \n    'default' => \n    array (\n      'database' => '$machine',\n      'username' => '$machine',\n      'password' => '$dbpw', \n      'host' => 'localhost', \n      'port' => '', \n      'driver' => 'mysql', \n      'prefix' => '', \n    ),\n  ),\n);~g" settings.php
 perl -pi -e "s~# .base_url = 'http://www.example.com';~\\\$base_url = 'http://$name\.cascadiaweb.net';~g" $www/$domain/sites/default/settings.php
-
+# Create virtual host file, enable and restart apache
 echo "<VirtualHost *:80>
         DirectoryIndex index.php
         DocumentRoot $www/$domain
@@ -37,7 +34,7 @@ echo "<VirtualHost *:80>
         ServerName $domain
 </VirtualHost>" > /etc/apache2/sites-available/$domain
 a2ensite $domain && service apache2 reload && service apache2 restart
-
+# Deploy site using Drush Make
 cd $www/$domain
 chown -R deploy:deploy $www/$domain
 chmod 775 $www/$domain
