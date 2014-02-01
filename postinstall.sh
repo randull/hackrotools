c0a113c63e7c27e1e4d9279a7a01e541e9f61ca5
Randall Puterbaugh <rputerbaugh@gmail.com>
2014-01-31 20:06:25 -0800

    Create postinstall.sh

---
+++ postinstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Prompt user to enter Domain Name
+read -p "Domain Name: " domain
+# Retrieve Variables from createsite.sh
+source createsite.sh
+echo $www
+cd $www/$domain/sites/default
+chmod 644 $www/$domain
