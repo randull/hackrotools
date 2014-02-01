7216fb3190b804a95f165cc7b06d7a30101ecdc8
Randall Puterbaugh <rputerbaugh@gmail.com>
2014-01-31 20:24:35 -0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -7,3 +7,6 @@ www=/var/www/drupal7
 # Change permissions for settings.php to 644
 cd $www/$domain/sites/default
 chmod 644 $www/$domain/sites/default/settings.php
+# Change temporary directory from /tmp to tmp
+db="UPDATE files SET file_directory_temp = REPLACE(file_directory_temp,'/tmp','tmp');"
+mysql -u deploy -e "$db"
