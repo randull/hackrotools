604a42bbff1c68785208e2bdbabe9aa8d902a62e
Randall Puterbaugh <rputerbaugh@gmail.com>
2014-01-31 20:09:01 -0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -3,7 +3,7 @@
 # Prompt user to enter Domain Name
 read -p "Domain Name: " domain
 # Retrieve Variables from createsite.sh
-source createsite.sh
+source https://raw.github.com/randull/createsite/master/createsite.sh
 echo $www
 cd $www/$domain/sites/default
-chmod 644 $www/$domain
+chmod 644 $www/$domain/sites/default/settings.php
