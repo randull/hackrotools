c674e674d3b2d311c83763fceaf9498a7a17b5cd
Randall Puterbaugh <rputerbaugh@gmail.com>
2014-01-31 20:11:23 -0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -2,8 +2,8 @@
 #
 # Prompt user to enter Domain Name
 read -p "Domain Name: " domain
-# Retrieve Variables from createsite.sh
-source https://raw.github.com/randull/createsite/master/createsite.sh
-echo $www
+# Create variables from Domain Name
+www=/var/www/drupal7
+# Change permissions for settings.php to 644
 cd $www/$domain/sites/default
 chmod 644 $www/$domain/sites/default/settings.php
