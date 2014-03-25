5684bec27f02b8bcce03b8a1857ee01ccbc3b288
Randall Puterbaugh <rputerbaugh@gmail.com>
2014-03-25 15:46:32 -0700

    Update createsite.sh

--- createsite.sh
+++ createsite.sh
@@ -8,7 +8,7 @@ read -p "Domain Name: " domain
 www=/var/www/drupal7
 tld=`echo $domain |rev |cut -f1 -d"." |rev`
 echo "\$tld = $tld"
-name=`echo $domain |rev |cut -c 5- |rev`
+name=`echo $domain |cut -f1 -d"."`
 echo "\$name = $name"
 shortname=`echo $name |cut -c -15`
 echo "\$shortname = $shortname"
