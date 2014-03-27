2e9ade07c04c07ae79ffa0a9a2dbea17c20201f7
Randall Puterbaugh <rputerbaugh@gmail.com>
2014-03-27 15:51:44 -0700

    Update removesite.sh

--- removesite.sh
+++ removesite.sh
@@ -24,6 +24,9 @@ service apache2 restart
 #
 rm $hosts/$domain
 echo "$hosts/$domain Virtualhost disabled and removed"
+# Notify user of MySQL password requirement
+#
+echo "MySQL verification required."
 #
 # Delete Database & User
 mysql -u deploy -p -e "drop database $machine;drop user $machine@localhost;"
