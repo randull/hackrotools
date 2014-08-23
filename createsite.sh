69eae0f091e0ab17d9e446143cc93b6d2449bdd2
Randall Puterbaugh <rputerbaugh@gmail.com>
2014-08-23 11:09:31 -0700

    Update createsite.sh

--- createsite.sh
+++ createsite.sh
@@ -42,10 +42,10 @@ mysql -u deploy -p -e "$db"
 ####                                                            ####
 ####    Create directories necessary for Drupal installation    ####
 ####                                                            ####
-sudo -u deploy mkdir $www/$domain $www/$domain/sites $www/$domain/sites/default $www/$domain/sites/default/files
-chmod a+w $www/$domain/sites/default/files
+sudo -u deploy mkdir $www/$domain $www/$domain/html $www/$domain/html/sites $www/$domain/html/sites/default $www/$domain/html/sites/default/files
+chmod a+w $www/$domain/html/sites/default/files
 chgrp -R www-data $www/$domain
-cd $www/$domain/sites/default/files
+cd $www/$domain/html/sites/default/files
 ####                                                            ####
 ####    Create Private directory and setup Backup directories   ####
 ####                                                            ####
@@ -55,7 +55,7 @@ chown -R deploy:www-data $www/$domain/private
 ####                                                            ####
 ####    Download favicon.ico                                    ####
 ####                                                            ####
-sudo -u deploy curl -o $www/$domain/sites/default/files/favicon.ico 'http://hackrobats.net/sites/default/files/favicon.ico'
+sudo -u deploy curl -o $www/$domain/html/sites/default/files/favicon.ico 'http://hackrobats.net/sites/default/files/favicon.ico'
 ####                                                            ####
 ####    Create log files and folders, as well as info.php       ####
 ####                                                            ####
@@ -63,8 +63,8 @@ sudo -u deploy mkdir $www/$domain/logs
 touch $www/$domain/logs/access.log $www/$domain/logs/error.log
 echo "<?php
         phpinfo();
-?>" > $www/$domain/info.php
-sudo chown deploy:www-data $www/$domain/info.php
+?>" > $www/$domain/html/info.php
+sudo chown deploy:www-data $www/$domain/html/info.php
 ####                                                            ####
 ####    Create virtual host file, enable and restart apache     ####
 ####                                                            ####
@@ -74,7 +74,7 @@ echo "<VirtualHost *:80>
         ServerAlias $domain *.$domain 
         ServerAlias $name.510interactive.com $name.hackrobats.net
         ServerAlias $name.5ten.co $name.cascadiacollective.net $name.cascadiaweb.net
-        DocumentRoot $www/$domain
+        DocumentRoot $www/$domain/html
         ErrorLog $www/$domain/logs/error.log
         CustomLog $www/$domain/logs/access.log combined
         DirectoryIndex index.php
@@ -83,7 +83,7 @@ a2ensite $domain && service apache2 reload
 ####                                                            ####
 ####    Create site structure using Drush Make                  ####
 ####                                                            ####
-cd $www/$domain
+cd $www/$domain/html
 drush make https://raw.github.com/randull/createsite/master/createsite.make -y
 ####                                                            ####
 ####    Deploy site using Drush Site-Install                    ####
@@ -92,9 +92,9 @@ drush si createsite --db-url="mysql://$machine:$dbpw@localhost/$machine" --site-
 ####                                                            ####
 ####    Remove Drupal Install files after installation          ####
 ####                                                            ####
-cd $www/$domain
+cd $www/$domain/html
 rm CHANGELOG.txt COPYRIGHT.txt install.php INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt README.txt UPGRADE.txt
-cd $www/$domain/sites
+cd $www/$domain/html/sites
 rm README.txt all/modules/README.txt all/themes/README.txt
 sudo chown -R deploy:www-data all/libraries/plupload
 rm -R all/libraries/plupload/examples
@@ -108,7 +108,7 @@ drush omega-export "omega_$machine" --revert -y
 ####                                                            ####
 ####    Initialize Git directory                                ####
 ####                                                            ####
-cd $www/$domain
+cd $www/$domain/html
 sudo -u deploy git init
 ####                                                            ####
 ####    Set owner of entire directory to deploy:www-data        ####
