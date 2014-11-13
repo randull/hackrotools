cb2d6af36da5b52643f95ca9fe64cec58e7bbff9
Randall Puterbaugh <rputerbaugh@gmail.com>
2014-11-12 19:34:57 -0800

    Update preparesite.sh

--- preparesite.sh
+++ preparesite.sh
@@ -5,7 +5,6 @@ read -p "Site Name: " sitename
 ####    Prompt user to enter Domain Name                        ####
 read -p "Domain Name: " domain
 ####    Create variables from Domain Name                       ####
-www=/var/www
 tld=`echo $domain  |cut -d"." -f2,3`
 name=`echo $domain |cut -f1 -d"."`
 shortname=`echo $name |cut -c -16`
@@ -19,32 +18,21 @@ echo "MySQL verification required."
 db="CREATE DATABASE IF NOT EXISTS $machine;GRANT ALL PRIVILEGES ON $machine.* TO $machine@localhost IDENTIFIED BY '$dbpw';FLUSH PRIVILEGES;"
 mysql -u deploy -p -e "$db"
 ####    Create directories necessary for Drupal installation    ####
-sudo -u deploy mkdir $www/$domain $www/$domain/html $www/$domain/html/sites $www/$domain/html/sites/default $www/$domain/html/sites/default/files
-chmod a+w $www/$domain/html/sites/default/files
-chgrp -R www-data $www/$domain
-cd $www/$domain/html/sites/default/files
-####    Create Private directory and setup Backup directories   ####
-sudo -u deploy mkdir -p $www/$domain/private/backup_migrate/scheduled $www/$domain/private/backup_migrate/manual
-chmod 6774 -R $www/$domain/private
-chown -R deploy:www-data $www/$domain/private
-####    Download favicon.ico                                    ####
-sudo -u deploy curl -o $www/$domain/html/sites/default/files/favicon.ico 'http://hackrobats.net/sites/default/files/favicon.ico'
-####    Create Public & Temp files directory                           ####
-mkdir $www/$domain/public $www/$domain/tmp
-chmod 6775 -R $www/$domain/public $www/$domain/tmp
-chown -R deploy:www-data $www/$domain/public $www/$domain/tmp
-####    Create log files and folders, as well as info.php       ####
-sudo -u deploy mkdir $www/$domain/logs
-touch $www/$domain/logs/access.log $www/$domain/logs/error.log
+cd /var/www && sudo -u deploy mkdir $domain
+cd /var/www/$domain && sudo -u deploy mkdir html logs private public tmp
+cd /var/www/$domain/html && sudo -u deploy mkdir -p sites/default/files
+cd /var/www/$domain/logs && touch access.log error.log
+cd /var/www/$domain/public && sudo -u deploy curl -o /var/www/$domain/public/favicon.ico 'http://www.hackrobats.net/sites/default/files/favicon.ico'
+cd /var/www/$domain/private && sudo -u deploy mkdir -p backup_migrate/manual backup_migrate/scheduled
 ####    Create virtual host file, enable and restart apache     ####
 echo "<VirtualHost *:80>
         ServerAdmin maintenance@hackrobats.net
         ServerName www.$domain
         ServerAlias *.$domain $name.510interactive.com $name.hackrobats.net
         ServerAlias $name.5ten.co $name.cascadiaweb.net
-        DocumentRoot $www/$domain/html
-        ErrorLog $www/$domain/logs/error.log
-        CustomLog $www/$domain/logs/access.log combined
+        DocumentRoot /var/www/$domain/html
+        ErrorLog /var/www/$domain/logs/error.log
+        CustomLog /var/www/$domain/logs/access.log combined
         DirectoryIndex index.php
 </VirtualHost>
 <VirtualHost *:80>
