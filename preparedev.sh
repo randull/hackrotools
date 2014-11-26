7c1f7d1343981f98dd94b960a19835602a6fa767
Randall Puterbaugh <rputerbaugh@gmail.com>
2014-11-25 19:01:21 -0800

    Update preparedev.sh

--- preparedev.sh
+++ preparedev.sh
@@ -9,14 +9,20 @@ tld=`echo $domain  |cut -d"." -f2,3`
 name=`echo $domain |cut -f1 -d"."`
 shortname=`echo $name |cut -c -16`
 machine=`echo $shortname |tr '-' '_'`
-#dbpw=$(pwgen -n 16)
+dbpwdev=$(pwgen -n 16)
+dbpwtest=$(pwgen -n 16)
+dbpwprod=$(pwgen -n 16)
 ####    Print DB Password for reference                         ####
-#echo "$dbpw"
+echo "$dbpwdev"
+echo "$dbpwtest"
+echo "$dbpwprod"
 ####    Notify user of MySQL password requirement               ####
-#echo "MySQL verification required."
+echo "MySQL verification required."
 ####    Create database and user                                ####
-#db="CREATE DATABASE IF NOT EXISTS $machine;GRANT ALL PRIVILEGES ON $machine.* TO $machine@localhost IDENTIFIED BY '$dbpw';FLUSH PRIVILEGES;"
-#mysql -u deploy -p -e "$db"
+db="CREATE DATABASE IF NOT EXISTS $machine;GRANT ALL PRIVILEGES ON $machine.* TO $machine@localhost IDENTIFIED BY '$dbpwdev';"
+db="GRANT ALL PRIVILEGES ON $machine.* TO $machine@dev.hackrobats.net IDENTIFIED BY '$dbpwdev';FLUSH PRIVILEGES;"
+mysql -u deploy -p -e "$db"
+mysql -u deploy -p -e "$db2"
 ####    Create directories necessary for Drupal installation    ####
 cd /var/www && sudo -u deploy mkdir $domain
 cd /var/www/$domain && sudo -u deploy mkdir html logs private public tmp
@@ -27,7 +33,7 @@ cd /var/www/$domain && sudo chmod 6775 html logs public private tmp
 ####    Create virtual host file, enable and restart apache     ####
 echo "<VirtualHost *:80>
         ServerAdmin maintenance@hackrobats.net
-        ServerName www.$domain
+        ServerName dev.$domain
         ServerAlias *.$domain $name.510interactive.com $name.hackrobats.net
         ServerAlias $name.5ten.co $name.cascadiaweb.com $name.cascadiaweb.net
         DocumentRoot /var/www/$domain/html
@@ -37,12 +43,12 @@ echo "<VirtualHost *:80>
 </VirtualHost>
 <VirtualHost *:80>
         ServerName $domain
-        Redirect 301 / http://www.$domain
+        Redirect 301 / http://dev.$domain
 </VirtualHost>  " > /etc/apache2/sites-available/$machine.conf
 a2ensite $machine.conf && service apache2 reload
 ####    Create /etc/cron.hourly entry                           ####
 echo "#!/bin/bash
-/usr/bin/wget -O - -q -t 1 http://$domain/sites/all/modules/elysia_cron/cron.php?cron_key=$machine" > /etc/cron.hourly/$machine
+/usr/bin/wget -O - -q -t 1 http://dev.$domain/sites/all/modules/elysia_cron/cron.php?cron_key=$machine" > /etc/cron.hourly/$machine
 ####    Create Drush Aliases                                    ####
 echo "<?php
 \$aliases[\"dev\"] = array(
@@ -50,18 +56,93 @@ echo "<?php
   'remote-user' => 'deploy',
   'root' => '/var/www/$domain/html',
   'uri' => 'dev.$domain',
-);
-\$aliases[\"prod\"] = array(
-  'remote-host' => 'prod.hackrobats.net',
-  'remote-user' => 'deploy',
-  'root' => '/var/www/$domain/html',
-  'uri' => 'www.$domain',
+  '#name' => '$machine.dev',
+  '#file' => '/home/deploy/.drush/$machine.aliases.drushrc.php',
+  'path-aliases' => 
+  array (
+    '%drush' => '/usr/share/php/drush',
+    '%dump-dir' => '/var/www/$domain/tmp',
+    '%files' => '/var/www/$domain/public',
+    '%site' => 'sites/default/',
+  ),
+  'databases' =>
+  array (
+    'default' =>
+    array (
+      'default' =>
+      array (
+        'database' => '$machine',
+        'username' => '$machine',
+        'password' => '$dbpwdev',
+        'host' => 'dev.hackrobats.net',
+        'port' => '',
+        'driver' => 'mysql',
+        'prefix' => '',
+      ),
+    ),
+  ),
 );
 \$aliases[\"test\"] = array(
   'remote-host' => 'test.hackrobats.net',
   'remote-user' => 'deploy',
   'root' => '/var/www/$domain/html',
   'uri' => 'test.$domain',
+  '#name' => '$machine.test',
+  '#file' => '/home/deploy/.drush/$machine.aliases.drushrc.php',
+  'path-aliases' => 
+  array (
+    '%drush' => '/usr/share/php/drush',
+    '%dump-dir' => '/var/www/$domain/tmp',
+    '%files' => '/var/www/$domain/public',
+    '%site' => 'sites/default/',
+  ),
+  'databases' =>
+  array (
+    'default' =>
+    array (
+      'default' =>
+      array (
+        'database' => '$machine',
+        'username' => '$machine',
+        'password' => '$dbpwtest',
+        'host' => 'test.hackrobats.net',
+        'port' => '',
+        'driver' => 'mysql',
+        'prefix' => '',
+      ),
+    ),
+  ),
+);
+\$aliases[\"prod\"] = array(
+  'remote-host' => 'prod.hackrobats.net',
+  'remote-user' => 'deploy',
+  'root' => '/var/www/$domain/html',
+  'uri' => 'www.$domain',
+  '#name' => '$machine.dev',
+  '#file' => '/home/deploy/.drush/$machine.aliases.drushrc.php',
+  'path-aliases' => 
+  array (
+    '%drush' => '/usr/share/php/drush',
+    '%dump-dir' => '/var/www/$domain/tmp',
+    '%files' => '/var/www/$domain/public',
+    '%site' => 'sites/default/',
+  ),
+  'databases' =>
+  array (
+    'default' =>
+    array (
+      'default' =>
+      array (
+        'database' => '$machine',
+        'username' => '$machine',
+        'password' => '$dbpwprod',
+        'host' => 'prod.hackrobats.net',
+        'port' => '',
+        'driver' => 'mysql',
+        'prefix' => '',
+      ),
+    ),
+  ),
 );" > /home/deploy/.drush/$machine.aliases.drushrc.php
 sudo chmod 664  /home/deploy/.drush/$machine.aliases.drushrc.php
 sudo chown deploy:www-data /home/deploy/.drush/$machine.aliases.drushrc.php
