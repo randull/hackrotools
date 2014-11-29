ce11799fe36452a3b5aeb7f9a8f7c870553c336f
Randall Puterbaugh <rputerbaugh@gmail.com>
2014-11-28 19:38:10 -0800

    Update preparesite.sh

--- preparesite.sh
+++ preparesite.sh
@@ -196,12 +196,12 @@ drush cc all && drush updb -y && drush cron
 
 
 ####    Create DB & user on Production                          ####
-db3="CREATE DATABASE IF NOT EXISTS $machine;GRANT ALL PRIVILEGES ON $machine.* TO $machine@localhost IDENTIFIED BY "'"$dbpw"'";"
+db3="CREATE DATABASE IF NOT EXISTS $machine;GRANT ALL PRIVILEGES ON $machine.* TO $machine@localhost IDENTIFIED BY '$dbpw';"
 db4="GRANT ALL PRIVILEGES ON $machine.* TO $machine@prod.hackrobats.net IDENTIFIED BY '$dbpw';FLUSH PRIVILEGES;"
 echo $db3
 ssh deploy@prod.hackrobats.net mysql -u deploy -e '$db3'
 echo $db4
-ssh deploy@prod.hackrobats.net "mysql -u deploy -e '$db4'"
+ssh deploy@prod.hackrobats.net mysql -u deploy -e '$db4'
 ####    Clone site directory to Production                      ####
 sudo -u deploy rsync -avzh /var/www/$domain/ deploy@prod.hackrobats.net:/var/www/$domain/
 ####    Clone Drush aliases                                     ####
