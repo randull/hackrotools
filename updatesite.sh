59fecb3c08fd3505f8bd4d0f3b1f0f6af29c8058
Randall Puterbaugh <rputerbaugh@gmail.com>
2014-12-17 16:06:00 -0800

    Update updatesite.sh

--- updatesite.sh
+++ updatesite.sh
@@ -23,10 +23,11 @@ sudo -u deploy ssh deploy@prod "cd /var/www/$domain/html && git push origin mast
 # Git steps on Development
 cd /var/www/$domain/html
 echo "Navigated to /var/www/$domain/html"
+ls -al
 git pull origin master
 echo "Pulled most recent version from Github"
 # Rsync steps for sites/default/files
-drush -y rsync -avz @$machine.prod:%files @$machine.dev:%files
+drush -y rsync -avz @$machine.prod:%files @$machine.dev:%files -y
 # Export DB from Prod to Dev using Drush
 drush sql-sync @$machine.prod @$machine.dev -y
 # Prepare site for Maintenance
