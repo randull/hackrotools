d1f45124a2c76ed4547360b2df6bae46ba0131f3
Randall Puterbaugh <rputerbaugh@gmail.com>
2016-01-08 14:28:21 -0800

    Added missing cd

--- scripts/hackrobats/pullprod.sh
+++ scripts/hackrobats/pullprod.sh
@@ -28,9 +28,13 @@ drush -y @$machine.prod cc all
 sudo -u deploy ssh deploy@prod "cd /var/www/$domain && sudo chmod -R ug=rw,o=r,a+X public/* tmp/*"
 sudo -u deploy ssh deploy@prod "cd /var/www/$domain && sudo chmod -R u=rw,go=r,a+X html/* logs/* private/*"
 # Fix File and Directory Permissions on Local
+cd /var/www/$domain/html
+sudo -u deploy rm -f CHANGELOG.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt README.txt UPGRADE.txt
+cd /var/www/$domain/html/sites
+sudo -u deploy rm -f example.sites.php README.txt all/modules/README.txt all/themes/README.txt default/default.settings.php
 cd /var/www/$domain
 sudo chmod -R ug=rw,o=r,a+X public/* tmp/*
-sudo chmod -R u=rw,go=r,a+X html/* logs/* private/* 
+sudo chmod -R u=rw,go=r,a+X html/* logs/* private/*
 # Checkout all changes on Local Environment
 cd /var/www/$domain/html
 git status
