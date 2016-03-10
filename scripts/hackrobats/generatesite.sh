2edac2ef46d43eed4cb677dba3ea9aeef28ada49
Randall Puterbaugh <rputerbaugh@gmail.com>
2016-03-09 18:05:08 -0800

    preparesite

--- scripts/hackrobats/generatesite.sh
+++ scripts/hackrobats/generatesite.sh
@@ -36,7 +36,7 @@ echo "Password Matches"
 hosts=/etc/apache2/sites-available    # Set variable for Apache Host config
 www=/var/www                          # Set variable for Drupal root directory
 tld=`echo $domain  |cut -d"." -f2,3`  # Generate tld (eg .com)
-name=`echo $domain |cut -f1 -d"."`    # Remove last for characters (eg .com) 
+name=`echo $domain |cut -f1 -d"."`    # Remove last four characters (eg .com) 
 longname=`echo $name |tr '-' '_'`     # Change hyphens (-) to underscores (_)
 shortname=`echo $name |cut -c -16`    # Shorten name to 16 characters for MySQL
 machine=`echo $shortname |tr '-' '_'` # Replace hyphens in shortname to underscores
