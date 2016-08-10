19368ebecf312b477a8c9df0b0c04cf557a05b26
Randall Puterbaugh <rputerbaugh@gmail.com>
2016-08-10 16:36:48 -0700

    Added chown command

--- scripts/hackrobats/pushdev.sh
+++ scripts/hackrobats/pushdev.sh
@@ -38,7 +38,6 @@ cd /var/www/$domain/html
 if [ -d "$www/$domain/README.md" ]
 then
   sudo mv README.md readme.md
-  echo "README.md has already been changed to readme.md"
 fi
 sudo -u deploy rm -f CHANGELOG.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt README.txt UPGRADE.txt
 cd /var/www/$domain/html/sites
