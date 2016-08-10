8dcdd10f2f51c2b3e99dfe3747dab15d161cffe3
Randall Puterbaugh <rputerbaugh@gmail.com>
2016-08-10 16:04:09 -0700

    Added chown command

--- scripts/hackrobats/pushdev.sh
+++ scripts/hackrobats/pushdev.sh
@@ -36,6 +36,9 @@ drush -y @$machine.dev cc all
 # Fix File and Directory Permissions on Local
 cd /var/www/$domain/html
 sudo mv README.md readme.md
+if [ -d "$www/$domain/README\.md" ]; then
+  echo "README.md has already been changed to readme.md"
+fi
 sudo mv README.txt readme.md
 sudo -u deploy rm -f CHANGELOG.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt README.txt UPGRADE.txt
 cd /var/www/$domain/html/sites
