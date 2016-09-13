b849532f586872dff91591e4cd8c1e0ee3aa362a
Randall Puterbaugh <rputerbaugh@gmail.com>
2016-09-13 13:35:38 -0700

    Added change directory

--- scripts/hackrobats/pullprod.sh
+++ scripts/hackrobats/pullprod.sh
@@ -20,6 +20,7 @@ longname=`echo $name |tr '-' '_'`     # Change hyphens (-) to underscores (_)
 shortname=`echo $name |cut -c -16`    # Shorten name to 16 characters for MySQL
 machine=`echo $shortname |tr '-' '_'` # Replace hyphens in shortname to underscores
 # Put Local & Prod sites into Maintenance Mode
+cd /var/www/$domain/html
 drush -y @$machine.local vset maintenance_mode 1
 drush -y @$machine.local cc all
 drush -y @$machine.prod vset maintenance_mode 1
