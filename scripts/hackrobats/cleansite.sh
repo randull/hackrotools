a33eaade6d2fd12392a1df44b7939ce21de10ccf
Randall Puterbaugh <rputerbaugh@gmail.com>
2016-09-14 15:22:16 -0700

    Added more Docroot cleanup

--- scripts/hackrobats/cleansite.sh
+++ scripts/hackrobats/cleansite.sh
@@ -1,1 +1,30 @@
-stuff
+#!/bin/bash
+#
+# This script deletes virtual hosts and drupal directory.
+#
+# Retrieve Domain Name from command line argument OR Prompt user to enter  
+if [ "$1" == "" ]; 
+  then
+    echo "No arguments provided";
+    read -p "Site domain to update: " domain;
+  else
+    echo $1;
+    domain=$1;
+fi
+# Create variables from Domain Name
+hosts=/etc/apache2/sites-available    # Set variable for Apache Host config
+www=/var/www                          # Set variable for Drupal root directory
+tld=`echo $domain  |cut -d"." -f2,3`  # Generate tld (eg .com)
+name=`echo $domain |cut -f1 -d"."`    # Remove last for characters (eg .com) 
+longname=`echo $name |tr '-' '_'`     # Change hyphens (-) to underscores (_)
+shortname=`echo $name |cut -c -16`    # Shorten name to 16 characters for MySQL
+machine=`echo $shortname |tr '-' '_'` # Replace hyphens in shortname to underscores
+# Fix File and Directory Permissions on Local
+cd /var/www/$domain/html
+sudo rm -rf modules/README.txt profiles/README.txt themes/README.txt
+sudo rm -rf CHANGELOG.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt UPGRADE.txt
+sudo rm -rf sites/README.txt sites/example.sites.php sites/all/libraries/plupload/examples sites/all/modules/README.txt sites/all/themes/README.txt sites/default/default.settings.php
+cd /var/www/$domain
+sudo chown -R deploy:www-data html/* logs/* private/* public/* tmp/*
+sudo chmod -R ug=rw,o=r,a+X public/* tmp/*
+sudo chmod -R u=rw,go=r,a+X html/* logs/* private/*
