96103107cf2b68b77ee8e585d774b77b61ac434f
Randall Puterbaugh <rputerbaugh@gmail.com>
2016-09-14 16:54:51 -0700

    Fixing permissions

--- scripts/hackrobats/cleansite.sh
+++ scripts/hackrobats/cleansite.sh
@@ -12,12 +12,12 @@ if [ "$1" == "" ];
     domain=$1;
 fi
 # Fix File and Directory Permissions on Local
-cd /var/www/$domain/html
-sudo -u deploy rm -rf modules/README.txt profiles/README.txt themes/README.txt
-sudo -u deploy rm -rf CHANGELOG.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt UPGRADE.txt
-sudo -u deploy rm -rf sites/README.txt sites/example.sites.php sites/all/libraries/plupload/examples sites/all/modules/README.txt sites/all/themes/README.txt sites/default/default.settings.php
+#cd /var/www/$domain/html
+#sudo -u deploy rm -rf modules/README.txt profiles/README.txt themes/README.txt
+#sudo -u deploy rm -rf CHANGELOG.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt UPGRADE.txt
+#sudo -u deploy rm -rf sites/README.txt sites/example.sites.php sites/all/libraries/plupload/examples sites/all/modules/README.txt sites/all/themes/README.txt sites/default/default.settings.php
 cd /var/www/$domain
 sudo -u deploy chown -R deploy:www-data *
-sudo -u deploy chmod -R u=rw,go=r,a+X html logs html/* logs/*
-sudo -u deploy chmod -R ug=rw,o=r,a+X private public tmp private/* public/* tmp/*
-sudo -u deploy chmod 775 html logs private public tmp
+#sudo -u deploy chmod -R u=rw,go=r,a+X html logs html/* logs/*
+#sudo -u deploy chmod -R ug=rw,o=r,a+X private public tmp private/* public/* tmp/*
+#sudo -u deploy chmod 775 html logs private public tmp
