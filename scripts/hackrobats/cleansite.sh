13ac10dcf34f4ef1ae887d79369c4741fffaeb9a
Randall Puterbaugh <rputerbaugh@gmail.com>
2016-09-14 15:23:35 -0700

    Added more Docroot cleanup

--- scripts/hackrobats/cleansite.sh
+++ scripts/hackrobats/cleansite.sh
@@ -11,14 +11,6 @@ if [ "$1" == "" ];
     echo $1;
     domain=$1;
 fi
-# Create variables from Domain Name
-hosts=/etc/apache2/sites-available    # Set variable for Apache Host config
-www=/var/www                          # Set variable for Drupal root directory
-tld=`echo $domain  |cut -d"." -f2,3`  # Generate tld (eg .com)
-name=`echo $domain |cut -f1 -d"."`    # Remove last for characters (eg .com) 
-longname=`echo $name |tr '-' '_'`     # Change hyphens (-) to underscores (_)
-shortname=`echo $name |cut -c -16`    # Shorten name to 16 characters for MySQL
-machine=`echo $shortname |tr '-' '_'` # Replace hyphens in shortname to underscores
 # Fix File and Directory Permissions on Local
 cd /var/www/$domain/html
 sudo rm -rf modules/README.txt profiles/README.txt themes/README.txt
