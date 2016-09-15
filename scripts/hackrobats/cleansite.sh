5d30c837cde043564e7ae1cb5ef49dce9f66685c
Randall Puterbaugh <rputerbaugh@gmail.com>
2016-09-14 17:07:42 -0700

    Fixing permissions

--- scripts/hackrobats/cleansite.sh
+++ scripts/hackrobats/cleansite.sh
@@ -12,22 +12,19 @@ if [ "$1" == "" ];
     domain=$1;
 fi
 # Fix File and Directory Permissions on Local
-#cd /var/www/$domain/html
-#sudo -u deploy rm -rf modules/README.txt profiles/README.txt themes/README.txt
-#sudo -u deploy rm -rf CHANGELOG.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt UPGRADE.txt
-#sudo -u deploy rm -rf sites/README.txt sites/example.sites.php sites/all/libraries/plupload/examples sites/all/modules/README.txt sites/all/themes/README.txt sites/default/default.settings.php
+cd /var/www/$domain/html
+sudo -u deploy rm -rf modules/README.txt profiles/README.txt themes/README.txt
+sudo -u deploy rm -rf CHANGELOG.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt UPGRADE.txt
+sudo -u deploy rm -rf sites/README.txt sites/example.sites.php sites/all/libraries/plupload/examples sites/all/modules/README.txt sites/all/themes/README.txt sites/default/default.settings.php
+echo "Unecessary files removed"
 cd /var/www/$domain
 sudo -u deploy chown -R deploy:www-data *
 sudo -u deploy chown -R deploy:www-data html logs private public tmp
 echo "File Ownership changed"
 sudo -u deploy chmod -R u=rw,go=r,a+X html/*
-echo "File Permissions changed for html"
 sudo -u deploy chmod -R u=rw,go=r,a+X logs/*
-echo "File Permissions changed for logs"
 sudo -u deploy chmod -R ug=rw,o=r,a+X private/*
-echo "File Permissions changed for private"
 sudo -u deploy chmod -R ug=rw,o=r,a+X public/*
-echo "File Permissions changed for public"
 sudo -u deploy chmod -R ug=rw,o=r,a+X tmp/*
-echo "File Permissions changed for tmp"
 sudo -u deploy chmod 775 html logs private public tmp
+echo "File Permissions changed"
