edaf738e16a5e7f726d7825a173fc6cd898cc87e
Randall Puterbaugh <rputerbaugh@gmail.com>
2016-09-14 17:46:01 -0700

    Fixing permissions

--- scripts/hackrobats/pullprod.sh
+++ scripts/hackrobats/pullprod.sh
@@ -32,21 +32,23 @@ sudo -u deploy ssh deploy@prod "cd /var/www/$domain && sudo chmod -R u=rw,go=r,a
 sudo -u deploy ssh deploy@prod "cd /var/www/$domain && sudo chmod -R ug=rw,o=r,a+X private/* public/* tmp/*"
 sudo -u deploy ssh deploy@prod "cd /var/www/$domain && sudo chmod 775 html logs private public tmp"
 # Remove unecessary files
-cd /var/www/$domain/html
-sudo -u deploy rm -rf modules/README.txt profiles/README.txt themes/README.txt
-sudo -u deploy rm -rf CHANGELOG.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt UPGRADE.txt
-sudo -u deploy rm -rf sites/README.txt sites/example.sites.php sites/all/libraries/plupload/examples sites/all/modules/README.txt sites/all/themes/README.txt sites/default/default.settings.php
-echo "Unecessary files removed"
 # Fix file ownership
 cd /var/www/$domain
-sudo -u deploy chown -R deploy:www-data *
-sudo -u deploy chown -R deploy:www-data html logs private public tmp
+sudo chown -Rf deploy:www-data *
+sudo chown -Rf deploy:www-data html/* logs/* private/* public/* tmp/*
 echo "File Ownership fixed"
 # Fix file permissions
-sudo -u deploy chmod -R u=rw,go=r,a+X html/* logs/*
-sudo -u deploy chmod -R ug=rw,o=r,a+X private/* public/* tmp/*
-sudo -u deploy chmod 775 html logs private public tmp
+sudo chmod -Rf u=rw,go=r,a+X html/* logs/*
+sudo chmod -Rf ug=rw,o=r,a+X private/* public/* tmp/*
+sudo chmod 775 *
+sudo chmod 644 html/.htaccess private/.htaccess public/.htaccess tmp/.htaccess
 echo "File Permissions fixed"
+# Remove unecessary files
+cd /var/www/$domain/html
+sudo rm -rf modules/README.txt profiles/README.txt themes/README.txt
+sudo rm -rf CHANGELOG.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt UPGRADE.txt
+sudo rm -rf sites/README.txt sites/example.sites.php sites/all/libraries/plupload/examples sites/all/modules/README.txt sites/all/themes/README.txt sites/default/default.settings.php
+echo "Unecessary files removed"
 # Checkout all changes on Local Environment
 cd /var/www/$domain/html
 git status
