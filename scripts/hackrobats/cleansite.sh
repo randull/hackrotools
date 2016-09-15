ffcab574246de3561b99794bc09c019ded346f7f
Randall Puterbaugh <rputerbaugh@gmail.com>
2016-09-14 17:24:36 -0700

    Fixing permissions

--- scripts/hackrobats/cleansite.sh
+++ scripts/hackrobats/cleansite.sh
@@ -13,17 +13,17 @@ if [ "$1" == "" ];
 fi
 # Remove unecessary files
 cd /var/www/$domain/html
-sudo -u deploy rm -rf modules/README.txt profiles/README.txt themes/README.txt
-sudo -u deploy rm -rf CHANGELOG.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt UPGRADE.txt
-sudo -u deploy rm -rf sites/README.txt sites/example.sites.php sites/all/libraries/plupload/examples sites/all/modules/README.txt sites/all/themes/README.txt sites/default/default.settings.php
+sudo rm -rf modules/README.txt profiles/README.txt themes/README.txt
+sudo rm -rf CHANGELOG.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt UPGRADE.txt
+sudo rm -rf sites/README.txt sites/example.sites.php sites/all/libraries/plupload/examples sites/all/modules/README.txt sites/all/themes/README.txt sites/default/default.settings.php
 echo "Unecessary files removed"
 # Fix file ownership
 cd /var/www/$domain
-sudo -u deploy chown -R deploy:www-data *
-sudo -u deploy chown -R deploy:www-data html/* logs/* private/* public/* tmp/*
+sudo chown -R deploy:www-data *
+sudo chown -R deploy:www-data html/* logs/* private/* public/* tmp/*
 echo "File Ownership fixed"
 # Fix file permissions
-sudo -u deploy chmod -R u=rw,go=r,a+X html/* logs/*
-sudo -u deploy chmod -R ug=rw,o=r,a+X private/* public/* tmp/*
-sudo -u deploy chmod 775 html logs private public tmp
+sudo chmod -R u=rw,go=r,a+X html/* logs/*
+sudo chmod -R ug=rw,o=r,a+X private/* public/* tmp/*
+sudo chmod 775 html logs private public tmp
 echo "File Permissions fixed"
