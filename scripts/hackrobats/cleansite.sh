310ac62cc3fb8232e8c7f3c24fa5d1fd2349dc42
Randall Puterbaugh <rputerbaugh@gmail.com>
2016-09-14 17:00:08 -0700

    Fixing permissions

--- scripts/hackrobats/cleansite.sh
+++ scripts/hackrobats/cleansite.sh
@@ -18,6 +18,7 @@ fi
 #sudo -u deploy rm -rf sites/README.txt sites/example.sites.php sites/all/libraries/plupload/examples sites/all/modules/README.txt sites/all/themes/README.txt sites/default/default.settings.php
 cd /var/www/$domain
 sudo -u deploy chown -R deploy:www-data *
+sudo -u deploy chown -R deploy:www-data html logs private public tmp
 echo "File Ownership changed"
 sudo -u deploy chmod -R u=rw,go=r,a+X html/*
 echo "File Permissions changed for html"
