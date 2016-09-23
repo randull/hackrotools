e70cec1ece3ea285ad83af5a7ebceebe6125a73f
Randall Puterbaugh <rputerbaugh@gmail.com>
2016-09-22 21:00:48 -0700

    Updated Cleansite

--- scripts/hackrobats/cleansite.sh
+++ scripts/hackrobats/cleansite.sh
@@ -11,20 +11,52 @@ if [ "$1" == "" ];
     echo $1;
     domain=$1;
 fi
-# Fix file ownership
+# Fix file ownership on Local
 cd /var/www/$domain
 sudo chown -Rf deploy:www-data *
 sudo chown -Rf deploy:www-data html/* logs/* private/* public/* tmp/*
-echo "File Ownership fixed"
-# Fix file permissions
+echo "File Ownership fixed on Local"
+# Fix file permissions on Local
 sudo chmod -Rf u=rw,go=r,a+X html/* logs/*
 sudo chmod -Rf ug=rw,o=r,a+X private/* public/* tmp/*
 sudo chmod 775 *
 sudo chmod 644 html/.htaccess private/.htaccess public/.htaccess tmp/.htaccess
-echo "File Permissions fixed"
-# Remove unecessary files
+echo "File Permissions fixed on Local"
+# Remove unecessary files on Local
 cd /var/www/$domain/html
 sudo rm -rf modules/README.txt profiles/README.txt themes/README.txt
 sudo rm -rf CHANGELOG.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt UPGRADE.txt
 sudo rm -rf sites/README.txt sites/example.sites.php sites/all/libraries/plupload/examples sites/all/modules/README.txt sites/all/themes/README.txt sites/default/default.settings.php
-echo "Unecessary files removed"
+echo "Unecessary files removed on Local"
+# Fix file ownership on Dev
+sudo -u deploy ssh deploy@dev "cd /var/www/$domain && sudo chown -Rf deploy:www-data *"
+sudo -u deploy ssh deploy@dev "cd /var/www/$domain && sudo chown -Rf deploy:www-data  html/* logs/* private/* public/* tmp/*"
+echo "File Ownership fixed on Dev"
+# Fix file permissions on Dev
+sudo -u deploy ssh deploy@dev "cd /var/www/$domain && sudo chmod -Rf u=rw,go=r,a+X html/* logs/*"
+sudo -u deploy ssh deploy@dev "cd /var/www/$domain && sudo chmod -Rf ug=rw,o=r,a+X private/* public/* tmp/*"
+sudo -u deploy ssh deploy@dev "cd /var/www/$domain && sudo chmod 775 *"
+sudo -u deploy ssh deploy@dev "cd /var/www/$domain && sudo chmod 664 html/.htaccess private/.htaccess public/.htaccess tmp/.htaccess"
+echo "File Permissions fixed on Dev"
+# Remove unecessary files on Dev
+sudo -u deploy ssh deploy@dev "cd /var/www/$domain/html && sudo rm -rf modules/README.txt profiles/README.txt themes/README.txt"
+sudo -u deploy ssh deploy@dev "cd /var/www/$domain/html && sudo rm -rf CHANGELOG.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt UPGRADE.txt"
+sudo -u deploy ssh deploy@dev "cd /var/www/$domain/html && sudo rm -rf sites/README.txt sites/example.sites.php sites/all/libraries/plupload/examples sites/all/modules/README.txt sites/all/themes/README.txt sites/default/default.settings.php"
+echo "Unecessary files removed on Dev"
+# Fix file ownership on Prod
+sudo -u deploy ssh deploy@prod "cd /var/www/$domain && sudo chown -Rf deploy:www-data *"
+sudo -u deploy ssh deploy@prod "cd /var/www/$domain && sudo chown -Rf deploy:www-data  html/* logs/* private/* public/* tmp/*"
+echo "File Ownership fixed on Prod"
+# Fix file permissions on Prod
+sudo -u deploy ssh deploy@prod "cd /var/www/$domain && sudo chmod -Rf u=rw,go=r,a+X html/* logs/*"
+sudo -u deploy ssh deploy@prod "cd /var/www/$domain && sudo chmod -Rf ug=rw,o=r,a+X private/* public/* tmp/*"
+sudo -u deploy ssh deploy@prod "cd /var/www/$domain && sudo chmod 775 *"
+sudo -u deploy ssh deploy@prod "cd /var/www/$domain && sudo chmod 664 html/.htaccess private/.htaccess public/.htaccess tmp/.htaccess"
+echo "File Permissions fixed on Prod"
+# Remove unecessary files on Prod
+sudo -u deploy ssh deploy@prod "cd /var/www/$domain/html && sudo rm -rf modules/README.txt profiles/README.txt themes/README.txt"
+sudo -u deploy ssh deploy@prod "cd /var/www/$domain/html && sudo rm -rf CHANGELOG.txt COPYRIGHT.txt INSTALL.mysql.txt INSTALL.pgsql.txt INSTALL.sqlite.txt INSTALL.txt LICENSE.txt MAINTAINERS.txt UPGRADE.txt"
+sudo -u deploy ssh deploy@prod "cd /var/www/$domain/html && sudo rm -rf sites/README.txt sites/example.sites.php sites/all/libraries/plupload/examples sites/all/modules/README.txt sites/all/themes/README.txt sites/default/default.settings.php"
+echo "Unecessary files removed on Prod"
+
+
