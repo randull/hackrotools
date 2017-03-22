be43247edd33c8fa63cf5c7d26b6c5fe3904b255
Randall Puterbaugh <rputerbaugh@gmail.com>
2017-03-22 11:11:45 -0700

    Fixed jquery versions

--- generatesite.sh
+++ generatesite.sh
@@ -226,7 +226,8 @@ drush vset error_level 0
 drush vset file_private_path /var/www/$domain/private
 drush vset file_temporary_path /var/www/$domain/tmp
 drush vset jquery_update_jquery_cdn "google"
-drush vset jquery_update_jquery_version "1.8"
+drush vset jquery_update_jquery_version "1.9"
+drush vset jquery_update_jquery_admin_version "1.8"
 drush vset prod_check_sitemail "maintenance@hackrobats.net"
 drush vset maintenance_mode 1
 
