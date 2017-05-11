4aa4b8071d6820f5f06160cfc71768f0a4d11821
Randall Puterbaugh <rputerbaugh@gmail.com>
2017-05-11 15:20:45 -0700

    Git checkout dev

--- pushdev.sh
+++ pushdev.sh
@@ -68,7 +68,7 @@ git push origin dev
 # Git steps on Dev
 sudo -u deploy ssh deploy@dev "cd /var/www/$domain/html && git status"
 sudo -u deploy ssh deploy@dev "cd /var/www/$domain/html && git add . -A"
-sudo -u deploy ssh deploy@dev "cd /var/www/$domain/html && git reset --hard --force origin/master"
+sudo -u deploy ssh deploy@dev "cd /var/www/$domain/html && git reset --hard origin/master"
 sudo -u deploy ssh deploy@dev "cd /var/www/$domain/html && git stash"
 sudo -u deploy ssh deploy@dev "cd /var/www/$domain/html && git stash drop"
 sudo -u deploy ssh deploy@dev "cd /var/www/$domain/html && git checkout dev"
